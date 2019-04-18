38475e60d7b405bf21f67032a17b31df5274fd47
feven <feventsegay6@gmail.com>
2019-04-18 17:02:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ def badge_maker(name)
  def batch_badge_creator(array)
    new_array=[]
   array.each do|name|
-  new_array.push("Hello, my name is #{name}. ")
+  new_array.push("Hello, my name is #{name}.")
 end
 return new_array
  end
