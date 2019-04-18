6a1ab1c066680a73dfab00db06a0d973b181ff6f
feven <feventsegay6@gmail.com>
2019-04-18 17:15:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ return new_array
    new_array=[]
    i=0
    array.each_with_index do|name,index|
-   new_array.push("Hello, #{name}! You'll be assigned to room #{num}!")
+   new_array.push("Hello, #{name}! You'll be assigned to room #{index}!")
    end
    return new_array
  end
