65ca2dc956b43b9676676ec6c42fcd4b37b2df0e
feven <feventsegay6@gmail.com>
2019-04-18 17:38:09 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,38 @@
 # Write your code here.
+def badge_maker(name)
+"Hello, my name is #{name}."
+ end
+ def batch_badge_creator(array)
+   new_array=[]
+  array.each do|name|
+  new_array.push("Hello, my name is #{name}.")
+end
+return new_array
+ end
+ def assign_rooms(array)
+   new_array=[]
+   index=1
+   array.each do|name|
+   new_array.push("Hello, #{name}! You'll be assigned to room #{index}!")
+   index +=1
+   end
+   return new_array
+ end
+ def printer(array)
+   batch_badge_creator(array).each do|num| 
+  puts  num
+ end
+   assign_rooms(array).each do|num|
+    puts num
+ end
+    end
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
